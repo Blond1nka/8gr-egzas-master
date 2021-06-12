7f544e6e470f6fd86a04f6ebd4968e2ff4993e33
Blond1nka <po.heize@gmail.com>
2021-06-12 10:36:52 +0300

    Initial commit

---
+++ javascript/main.js
@@ -0,0 +1,32 @@
+/* 
+Kiekviena užduotis turi būti aprašyta kaip atskira funkcija
+kuriai yra paduodami duomenys. Aprašykite visas funkcijas žemiau. 
+Visas funkcijas iškvieskite vieną po kitos, žemiau šio komentaro.
+
+Taip pat parašykite funkciją kuri: isikviesdama save pačią atspausdina skaičius nuo 1 iki 10
+
+*/
+
+
+
+
+
+function Kavines(data) {
+    let Kavines = document.getElementByName("kavine"); 
+    console.log(Kavines);
+}
+
+
+
+function KaunoKavines() {
+    
+}
+
+
+function PirmosKavinesPatiekalai() {
+    
+}
+
+
+
+
